496129f6ffef0164dd7bc4513c68953d429eb6e3
JayeshKarandeDev <jayesh.karande.dev@gmail.com>
2024-03-10 10:36:22 +0530

    html templates

---
+++ cmd/api/handlers/handlers.go
@@ -0,0 +1,62 @@
+package handler
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	// w.Write([]byte("Hellow from root"))
+	files := []string{"./ui/html/pages/home.tmpl.html", "./ui/html/pages/base.tmpl.html", "./ui/html/pages/nav.tmpl.html"}
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		// log.print(err.Error())
+		http.Error(w, "internal server error", 500)
+		return
+	}
+	err = t.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		// log.print(err.Error())
+		http.Error(w, "internal server error", 500)
+		return
+	}
+
+}
+
+func Variables(w http.ResponseWriter, r *http.Request) {
+	// type conversion
+	byteSlice := []byte{'H', 'e', 'l', 'l', 'o'}
+	byteSlice1 := []byte("Hellow from byte1")
+	fmt.Println("Slice of bytes:", byteSlice, byteSlice1)
+	fmt.Println("byte to string:", string(byteSlice), string(byteSlice1))
+	w.Write([]byte("check the output in terminal"))
+}
+
+func CreateSnippets(w http.ResponseWriter, r *http.Request) {
+	// this if check is useless now as we have added method POST in the routing as part of go 1.22
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	// If the method is POST, execute this block
+	w.Write([]byte("Create a new snippet..."))
+}
+
+func ViewSnippet(w http.ResponseWriter, r *http.Request) {
+	id1 := r.URL.Query().Get("id")
+	fmt.Println("id is", id1)
+	fmt.Printf("type is %T", id1)
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,41 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	handler "github.com/JayeshKarandeDev/microservice/cmd/api/handlers"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /", home)
-	mux.HandleFunc("GET /variables", variables)
-	mux.HandleFunc("POST /snippets", createSnippets)
+	mux.HandleFunc("GET /", handler.Home)
+	mux.HandleFunc("GET /variables", handler.Variables)
+	mux.HandleFunc("POST /snippets", handler.CreateSnippets)
+	mux.HandleFunc("GET /snippet/view", handler.ViewSnippet)
 	fmt.Println("Listing server on http://localhost:4000")
 	http.ListenAndServe(":4000", mux)
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hellow from root"))
-}
-
-func variables(w http.ResponseWriter, r *http.Request) {
-	// type conversion
-	byteSlice := []byte{'H', 'e', 'l', 'l', 'o'}
-	byteSlice1 := []byte("Hellow from byte1")
-	fmt.Println("Slice of bytes:", byteSlice, byteSlice1)
-	fmt.Println("byte to string:", string(byteSlice), string(byteSlice1))
-	w.Write([]byte("check the output in terminal"))
-}
-
-func createSnippets(w http.ResponseWriter, r *http.Request) {
-	// this if check is useless now as we have added method POST in the routing as part of go 1.22
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	// If the method is POST, execute this block
-	w.Write([]byte("Create a new snippet..."))
 }
